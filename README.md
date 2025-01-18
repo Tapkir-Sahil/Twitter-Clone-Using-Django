b05eec56a913dfbfd1bc410b963374c48e19b11f
sahil-tapkir <sahiltapkir07@gmail.com>
2025-01-18 23:00:53 +0530

    remove unused files and directories from the twitter_clone project

--- README.md
+++
@@ -1,2 +0,0 @@
-# Twitter-Clone-Using-Django
-A Basic Twitter Clone Using Django
--- twitter_clone/twitteruser/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TwitteruserConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'twitteruser'
